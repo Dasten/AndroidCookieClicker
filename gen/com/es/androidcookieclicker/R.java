7576b75c4aac96e0cb71c41166221c5e66c1c8e4
Carlos Belmonte <carlosbceniza@gmail.com>
2013-11-18 14:17:30 +0100

    Borrada la activity de TeamWork

--- gen/com/es/androidcookieclicker/R.java
+++ gen/com/es/androidcookieclicker/R.java
@@ -28,7 +28,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
-        public static final int activity_team_work=0x7f030001;
     }
     public static final class menu {
         public static final int game=0x7f070000;
@@ -37,9 +36,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050004;
-        public static final int teamWork=0x7f050002;
-        public static final int title_activity_game=0x7f050003;
+        public static final int hello_world=0x7f050003;
+        public static final int title_activity_game=0x7f050002;
     }
     public static final class style {
         /** 
---
+++ src/com/es/androidcookieclicker/LogicPowerUps.java
@@ -0,0 +1,5 @@
+package com.es.androidcookieclicker;
+
+public class LogicPowerUps {
+
+}
