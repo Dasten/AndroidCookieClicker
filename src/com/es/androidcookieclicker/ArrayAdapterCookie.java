3eb679c08a2a939da090958a221c5e3fd68a0a91
Pablo Fernandez <poltero@ovi.com>
2013-11-21 15:14:29 +0100

    GetView en arrayadapter (no terminado)

--- src/com/es/androidcookieclicker/ArrayAdapterCookie.java
+++ src/com/es/androidcookieclicker/ArrayAdapterCookie.java
@@ -4,6 +4,8 @@
 package com.es.androidcookieclicker;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 /**
@@ -17,6 +19,24 @@ public class ArrayAdapterCookie<Object> extends ArrayAdapter<Object> {
 	public ArrayAdapterCookie(Context context, int resource, Object[] objects) {
 		super(context, resource, objects);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public View getView(int position, View view, ViewGroup vg){
+		/*int countElements = vg.getChildCount();
+		
+		for(int i = 0; i < countElement)*/
+		
+		View row = super.getView(position, view, vg);
+		
+		if(!row.isEnabled()) {
+			row.setBackgroundColor(2);
+		} else {
+			row.setBackgroundColor(1);
+		}
+		
+		return row;
+		
 	}
 
 	@Override
